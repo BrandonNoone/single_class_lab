8d7e4d9ae0305526a33614d6b015847501f4ebc3
Brandon <Brandonrw97@gmail.com>
2023-07-11 09:25:46 +0100

    struggled with this

---
+++ runner.py
@@ -0,0 +1,7 @@
+from students import Students
+
+account = Students("Brandon", "e65", "python")
+
+print (account.student_name)
+print(account.cohort)
+print(account.favourite)
---
+++ students.py
@@ -0,0 +1,12 @@
+class Students:
+    def __init__(self, input_student_name, input_cohort,):
+        self.student_name = input_student_name
+        self.cohort = input_cohort
+
+def talk(self):
+    return "I can talk!"
+
+def say_fac
+
+# This really tripped me up, ive been watching some videos at home to try
+#with it and i think im slowly getting there 
